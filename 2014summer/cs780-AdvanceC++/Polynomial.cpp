8002b472561d5e1a46f5ad939b1bb306caee0982
Haijun Su <navysu@gmail.com>
2014-06-11 02:49:47 -0400

    ready for test

--- 2014summer/cs780-AdvanceC++/Polynomial.cpp
+++ 2014summer/cs780-AdvanceC++/Polynomial.cpp
@@ -6,11 +6,16 @@
 #include <vector>
 
 using namespace std; 
+string intToString (int a)
+{
+    ostringstream temp;
+    temp<<a;
+    return temp.str();
+}
 
 class Pair {
-	private:
-		int coe, exp;
 	public:
+		int coe, exp;
 		// default constructor
 		// coe and exp are zeros
 		Pair() {
@@ -39,6 +44,10 @@ class Pair {
 			coe = pair.coe;
 			exp = pair.exp;
 			return *this;
+		}
+		
+		string toString() {
+			return intToString(coe) + " " + intToString(exp);
 		}
 		// overloads << so we can directly print Pairs
 		friend ostream& operator<< (ostream& os, Pair pair);
@@ -82,7 +91,58 @@ node<int>* make_int_list(int n){
 	} 
 	return first; 
 } 
- 
+
+// create node list from int tokens
+node<Pair>* make_canonical_list(vector<int> tokens){ 
+	if(tokens.size()<=1) return 0; // at least 2 elements
+
+	node<Pair>* first,*p,*q,*tmp; 
+	Pair pair(tokens.at(0), tokens.at(1));
+	first=p=new node<Pair>(pair); 
+	for(int i=2; i<tokens.size(); ){
+		// check whether the pair can be merged
+		int merged = 0;
+		do {
+			if (p->data().exp == tokens.at(i + 1)) { // same exp
+				p->data().coe = p->data().coe + tokens.at(i);
+				merged = 1;
+				break;
+			}
+		} while (p=p->link());
+		// insert node in the list
+		if (merged == 0) { // add new node
+			Pair pair(tokens.at(i), tokens.at(i + 1));
+			q=new node<Pair>(pair); 
+			// insert new node
+			p = tmp = first;
+			if (p->data().exp < pair.exp) { // add q to first
+				q->link() = first;
+				first = q;
+				tmp = 0;
+			} else {
+				int added = 0;
+				while (p->link()) {
+					p = p->link();
+					if (p->data().exp < pair.exp) { // insert q before p
+						tmp->link() = q;
+						q->link() = p;
+						tmp = 0;
+						added = 1;
+						break;
+					}
+					tmp = p;	
+				}
+				if (added = 0) { // add to end
+					p->link() = q;
+				}
+			}
+		}
+		p = first;//move p to first
+		i += 2;
+	} 
+	return first; 
+} 
+
 template<class T> 
 void print_list_rec(node<T>* p){ 
 	if(p==0) return; 
@@ -102,6 +162,19 @@ void add_at_end(node<T>* &first, node<T>*r){
 
 } 
 
+template<class T> 
+string getNodePairString(node<T> p) {
+	node<T> q = p;
+	node<T> *q1;
+	string rtn = q.data().toString() + " "; 
+	while(q.link()) {
+		q1 = q.link();
+		rtn += q1->data().toString() + " "; 
+	}
+	delete q1;
+	return rtn;
+}
+
 // split words by space
 vector<int> splitLine(string line) {
 	string buf; 
@@ -113,80 +186,189 @@ vector<int> splitLine(string line) {
 	return tokens;
 } 
 
-template<class T> 
 class Polynomial {
 	private:
 		string original;
-		node<T> * p;
+		node<Pair> * p;
 	public:
 		// default constructor
 		Polynomial() {
-			original = NULL;
+			original = '\0';
 			p = NULL;
 		}
 		// 1 parameter constructor lets us write
 		// Polynomial x("10 20");
 		Polynomial(string equation) {
 			original = equation;
-			//TODO
+			vector<int> tokens = splitLine(equation);
+			p = make_canonical_list(tokens);
 		}
 		
 		// copy constructor for pass by value and
 		// initialization
 		Polynomial(const Polynomial & polynomial){
-			//TODO
+			original = polynomial.original;
+			node<Pair> * p1, *q, *tmp;
+			p1 = polynomial.p;
+			Pair pair = p1->data(); // copy data from polynomial to pair
+			p = tmp = new node<Pair>(pair);
+			while (p1->link()) {
+				p1 = p1->link();
+				Pair pair = p1->data();
+				q = new node<Pair>(pair);
+				tmp->link() = q;
+				tmp = q;
+			}
+			p1 = q = tmp = 0;
 		}
 		// destructor
 		~Polynomial(){
-			delete [] p;
+			delete p;
 		}
 		// overloaded assignment lets us assign
 		// one Polynomial to another
 		Polynomial & operator=(const Polynomial & polynomial){
-			//TODO
+			if(this==&polynomial)return *this;
+			delete p;
+			original = polynomial.original;
+			node<Pair> * p1, *q, *tmp;
+			p1 = polynomial.p;
+			Pair pair = p1->data(); // copy data from polynomial to pair
+			p = tmp = new node<Pair>(pair);
+			while (p1->link()) {
+				p1 = p1->link();
+				Pair pair = p1->data();
+				q = new node<Pair>(pair);
+				tmp->link() = q;
+				tmp = q;
+			}
+			p1 = q = tmp = 0;			
 			return *this;
 		}
+		// overloaded assignment lets us assign
+		// one Polynomial to another
+		Polynomial operator+(const Polynomial & polynomial){
+			node<Pair> * p1, *p2;
+			Polynomial plnm1(original);
+			Polynomial plnm2 = polynomial;
+			p1 = plnm1.p;
+			p2 = plnm2.p;
+			add_at_end(p1, p2);
+			string values = getNodePairString(*p1);
+			Polynomial plnm(values);
+			delete p1;
+			delete p2;
+			return plnm;
+		}
+		// overloaded assignment lets us assign
+		// one Polynomial to another
+		Polynomial operator-(const Polynomial & polynomial){
+			node<Pair> * p1, *p2, *p3;
+			Polynomial plnm1(original);
+			Polynomial plnm2 = polynomial;
+			p1 = plnm1.p;
+			p2 = p3 = plnm2.p;
+			p3->data().coe = 0 - p3->data().coe;
+			while (p3 = p3->link()) {
+				p3->data().coe = 0 - p3->data().coe;
+			}
+			add_at_end(p1, p2);
+			string values = getNodePairString(*p1);
+			Polynomial plnm(values);
+			delete p1;
+			delete p2;
+			delete p3;
+			return plnm;
+		}
+		// overloaded assignment lets us assign
+		// one Polynomial to another
+		Polynomial operator*(const Polynomial & polynomial){
+			node<Pair> * p1, *p2, *p3, *p4, *p5, * p6;
+			Polynomial plnm1(original);
+			Polynomial plnm2 = polynomial;
+			p1 = plnm1.p;
+			p2 = p3 = plnm2.p;
+			int coe = p1->data().coe * p3->data().coe;
+			int exp = p1->data().exp + p3->data().exp;
+			Pair pair(coe, exp);
+			p4 = p5 = new node<Pair>(pair);
+			while (p3 = p3->link()) {
+				coe = p1->data().coe * p3->data().coe;
+				exp = p1->data().exp + p3->data().exp;
+				Pair pair(coe, exp);
+				p6 = new node<Pair>(pair);
+				p5->link() = p6;
+				p5 = p6;
+			}
+			p3 = p2;
+			while(p1=p1->link()) {
+				do {
+					coe = p1->data().coe * p3->data().coe;
+					exp = p1->data().exp + p3->data().exp;
+					Pair pair(coe, exp);
+					p6 = new node<Pair>(pair);
+					p5->link() = p6;
+					p5 = p6;
+				} while (p3=p3->link());
+				p3 = p2;
+			}
+			string values = getNodePairString(*p4);
+			Polynomial plnm(values);
+			delete p1;
+			delete p2;
+			delete p3;
+			delete p4;
+			delete p5;
+			delete p6;
+			return plnm;
+		}
+		
+		string getCanonical() {
+			return getNodePairString(*p);
+		}
+		
+		string getOriginal() {
+			return original;
+		}
+		
 		// overloads << so we can directly print Pairs
-		friend ostream& operator<< <T>(ostream& os, Polynomial<T> polynomial);
+		friend ostream& operator<< (ostream& os, Polynomial polynomial);
 };
 
-template<class T> 
-ostream& operator<< (ostream& os, Polynomial<T> polynomial){
-	os<< polynomial.original << " " << print_list_rec(polynomial.p);
+
+ostream& operator<< (ostream& os, Polynomial polynomial){
+	os<< "original: " << polynomial.original << endl;
+	os<< "canonical: ";
+	node<Pair> * p = polynomial.p;
+	if(p>0){
+		do {
+			os<<p->data()<<" "; 
+		} while (p = p->link());
+	}
     return os;
 };
-int main(int argc, char *argv[]) 
-{ 
-	node<Pair>* first,*p,*q;
-	Pair pair(1,2);
-	first=p=new node<Pair>(pair); 
-	for(int i=2; i<=5; i++){ 
-		Pair pair(i,(i+2) % 4);
-		q=new node<Pair>(pair); 
-		p->link()=q; 
-		p=q; 
-	} 
-	print_list<Pair>(first);
-	//create and print a list of n integer nodes 
-	int n; 
-	cout<<"How many nodes? "; 
-	cin>>n; 
-	cout<<endl; 
-	print_list<int>(make_int_list(n)); 
-	cout<<endl; 
 
-	//now do it recursively 
+int main(int argc, char* argv[]){
+ 
+	if (argc < 3) { //first argument(argv[0]) is the program path
+		cerr << "usage: " << argv[0] << " <input file> <dest file>" << endl;
+		return 0;
+	}
+       
+	//note the arguments to main being used below
+	ifstream input(argv[1]); //we'll assume the file exists
+	ofstream output(argv[2]);
+	char ch;
+	input >> noskipws; //the noskipws flag allows for whitespace characters to be extracted
+    string line;
+	while (getline(input, line)){
+		Polynomial plnm(line);
+		output<< plnm << endl;
+	}
 
-	print_list_rec<int>(make_int_list(n)); 
-	cout<<endl; 
-
-	node<int>* a=make_int_list(9); 
-	node<int>* b=new node<int>(56); 
-	add_at_end(a,b); 
-	cout<<endl; 
-	print_list<int>(a); 
-
-
-	//system("PAUSE"); 
-	return EXIT_SUCCESS; 
+	output << endl;
+	input.close();
+	output << flush;
+	output.close();
+    return 0;
 }
