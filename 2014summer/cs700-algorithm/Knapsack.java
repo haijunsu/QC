0779a415015a7b5b54df0fd1f4fe6918585a72a9
Haijun Su <navysu@gmail.com>
2014-07-31 02:10:46 -0400

    implement main functions

--- 2014summer/cs700-algorithm/Knapsack.java
+++ 2014summer/cs700-algorithm/Knapsack.java
@@ -1,301 +1,276 @@
+import java.util.ArrayList;
+import java.util.List;
 
-import java.util.*;
-import java.text.*;
- 
+/**
+ * CS700 project: Knapsack: Given a set of items, each with a weight and a
+ * value, determine the number of each item to include in a collection so that
+ * the total weight is less than or equal to a given limit and the total value
+ * is as large as possible.
+ * 
+ * 
+ * 
+ * @author Haijun Su 2014 summer
+ *
+ */
 public class Knapsack {
- 
-    public Knapsack() {
-        ZeroOneKnapsack zok = new ZeroOneKnapsack(400); // 400 dkg = 400 dag = 4 kg
- 
-        // making the list of items that you want to bring
-        zok.add("map", 9, 150);
-        zok.add("compass", 13, 35);
-        zok.add("water", 153, 200);
-        zok.add("sandwich", 50, 160);
-        zok.add("glucose", 15, 60);
-        zok.add("tin", 68, 45);
-        zok.add("banana", 27, 60);
-        zok.add("apple", 39, 40);
-        zok.add("cheese", 23, 30);
-        zok.add("beer", 52, 10);
-        zok.add("suntan cream", 11, 70);
-        zok.add("camera", 32, 30);
-        zok.add("t-shirt", 24, 15);
-        zok.add("trousers", 48, 10);
-        zok.add("umbrella", 73, 40);
-        zok.add("waterproof trousers", 42, 70);
-        zok.add("waterproof overclothes", 43, 75);
-        zok.add("note-case", 22, 80);
-        zok.add("sunglasses", 7, 20);
-        zok.add("towel", 18, 12);
-        zok.add("socks", 4, 50);
-        zok.add("book", 30, 10);
- 
-        // calculate the solution:
-        List<Item> itemList = zok.calcSolution();
- 
-        // write out the solution in the standard output
-        if (zok.isCalculated()) {
-            NumberFormat nf  = NumberFormat.getInstance();
- 
-            System.out.println(
-                "Maximal weight           = " +
-                nf.format(zok.getMaxWeight() / 100.0) + " kg"
-            );
-            System.out.println(
-                "Total weight of solution = " +
-                nf.format(zok.getSolutionWeight() / 100.0) + " kg"
-            );
-            System.out.println(
-                "Total value              = " +
-                zok.getProfit()
-            );
-            System.out.println();
-            System.out.println(
-                "You can carry the following materials " +
-                "in the knapsack:"
-            );
-            for (Item item : itemList) {
-                if (item.getInKnapsack() == 1) {
-                    System.out.format(
-                        "%1$-23s %2$-3s %3$-5s %4$-15s \n",
-                        item.getName(),
-                        item.getWeight(), "dag  ",
-                        "(value = " + item.getValue() + ")"
-                    );
-                }
-            }
-        } else {
-            System.out.println(
-                "The problem is not solved. " +
-                "Maybe you gave wrong data."
-            );
-        }
- 
-    }
- 
-    public static void main(String[] args) {
-        new Knapsack();
-    }
- 
+
+	private static boolean isDebug = false;
+
+	/**
+	 * An item container which provides item to pack
+	 */
+	private List<Item> itemList;
+
+	/**
+	 * maximum capacity of pack
+	 */
+	private int maxWeight;
+
+	/**
+	 * total weight after packing items
+	 */
+	private int totalWeight;
+
+	/**
+	 * total values of items which have be packed
+	 */
+	private int benefit = 0;
+
+	/**
+	 * Init a pack with maximum capacity
+	 * 
+	 * @param weight
+	 */
+	public Knapsack(int weight) {
+		this.maxWeight = weight;
+		itemList = new ArrayList<Item>();
+	}
+
+	/**
+	 * add an item into item container
+	 * 
+	 * @param name
+	 * @param weight
+	 * @param value
+	 */
+	public void add(String name, int weight, int value) {
+		itemList.add(new Item(name, weight, value));
+	}
+
+	/**
+	 * Choose items and pack them
+	 * 
+	 * @return
+	 */
+	public List<Item> chooseItems() {
+		List<Item> packedList = new ArrayList<Item>();
+		int total = itemList.size();
+
+		if (total > 0 && maxWeight > 0) {
+			List<List<Integer>> iterates = new ArrayList<List<Integer>>();
+			List<Integer> currIter = new ArrayList<Integer>();
+
+			iterates.add(currIter);
+			for (int j = 0; j <= maxWeight; j++)
+				currIter.add(0); // first iterate
+			for (int i = 1; i <= total; i++) {
+				List<Integer> prevIter = currIter;
+				iterates.add(currIter = new ArrayList<Integer>());
+				for (int j = 0; j <= maxWeight; j++) {
+					if (j > 0) {
+						int weight = itemList.get(i - 1).getWeight();
+						// if weight is greater than j, value is previous
+						// iterator's value. else get the maximum value between
+						// previous iterator's value and current item value plus
+						// previous (j - weigh)'s value
+						currIter.add((weight > j) ? prevIter.get(j) : Math.max(
+								prevIter.get(j), itemList.get(i - 1).getValue()
+										+ prevIter.get(j - weight)));
+					} else {
+						// 0 items
+						currIter.add(0);
+					}
+				}
+			}
+			// the last one is the benefit
+			benefit = currIter.get(maxWeight);
+
+			// find which items have been packed in pack
+			for (int i = total, j = maxWeight; i > 0 && j >= 0; i--) {
+				int value1 = iterates.get(i).get(j);
+				int value2 = iterates.get(i - 1).get(j);
+				// compare with previous iterate's value. If it different, the
+				// item is packed
+				if ((i == 0 && value1 > 0) || (i > 0 && value1 != value2)) {
+					Item item = itemList.get(i - 1);
+					int weight = item.getWeight();
+					packedList.add(item);
+					j -= weight;
+					totalWeight += weight;
+				}
+			}
+		}
+		return packedList;
+	}
+
+	public int getBenefit() {
+		return benefit;
+	}
+
+	public int getTotalWeight() {
+		return totalWeight;
+	}
+
+	public int getMaxWeight() {
+		return maxWeight;
+	}
+	
+	public List<Item> getItemList() {
+		return itemList;
+	}
+
+	// print message to console
+	private static void log(String message) {
+		if (isDebug) {
+			System.out.println(message);
+		}
+	}
+
+	public static void main(String[] args) {
+		isDebug = "true".equalsIgnoreCase(System.getProperty("isDebug"));
+
+		Knapsack pack = new Knapsack(400); // 400
+		// making the list of items that you want to bring
+		pack.add("map", 9, 150);
+		pack.add("compass", 13, 35);
+		pack.add("water", 153, 200);
+		pack.add("sandwich", 50, 160);
+		pack.add("glucose", 15, 60);
+		pack.add("tin", 68, 45);
+		pack.add("banana", 27, 60);
+		pack.add("apple", 39, 40);
+		pack.add("cheese", 23, 30);
+		pack.add("beer", 52, 10);
+		pack.add("suntan cream", 11, 70);
+		pack.add("camera", 32, 30);
+		pack.add("t-shirt", 24, 15);
+		pack.add("trousers", 48, 10);
+		pack.add("umbrella", 73, 40);
+		pack.add("waterproof trousers", 42, 70);
+		pack.add("waterproof overclothes", 43, 75);
+		pack.add("note-case", 22, 80);
+		pack.add("sunglasses", 7, 20);
+		pack.add("towel", 18, 12);
+		pack.add("socks", 4, 50);
+		pack.add("book", 30, 10);
+
+		// choose items:
+		List<Item> itemList = pack.chooseItems();
+
+		// write out the result in the standard output
+
+		System.out.println("Maximal weight = " + pack.getMaxWeight());
+		System.out.println("Total weight of solution = "
+				+ pack.getTotalWeight());
+		System.out.println("Total value = " + pack.getBenefit());
+		System.out.println();
+		System.out.println("The following items has been choosed");
+		for (Item item : itemList) {
+			System.out.println(item.getName() + "(weight = " + item.getWeight()
+					+ ", value = " + item.getValue() + ")");
+		}
+		System.out.println("The following items has been choosed");
+		
+		System.out.println();
+		System.out.println("The following items are the total items which can be choosed for packing");
+		itemList = pack.getItemList();
+		for (Item item : itemList) {
+			System.out.println(item.getName() + "(weight = " + item.getWeight()
+					+ ", value = " + item.getValue() + ")");
+		}
+
+	}
+
 }
 
-class ZeroOneKnapsack {
-	 
-    protected List<Item> itemList  = new ArrayList<Item>();
-    protected int maxWeight        = 0;
-    protected int solutionWeight   = 0;
-    protected int profit           = 0;
-    protected boolean calculated   = false;
- 
-    public ZeroOneKnapsack() {}
- 
-    public ZeroOneKnapsack(int _maxWeight) {
-        setMaxWeight(_maxWeight);
-    }
- 
-    public ZeroOneKnapsack(List<Item> _itemList) {
-        setItemList(_itemList);
-    }
- 
-    public ZeroOneKnapsack(List<Item> _itemList, int _maxWeight) {
-        setItemList(_itemList);
-        setMaxWeight(_maxWeight);
-    }
- 
-    // calculte the solution of 0-1 knapsack problem with dynamic method:
-    public List<Item> calcSolution() {
-        int n = itemList.size();
- 
-        setInitialStateForCalculation();
-        if (n > 0  &&  maxWeight > 0) {
-            List< List<Integer> > c = new ArrayList< List<Integer> >();
-            List<Integer> curr = new ArrayList<Integer>();
- 
-            c.add(curr);
-            for (int j = 0; j <= maxWeight; j++)
-                curr.add(0);
-            for (int i = 1; i <= n; i++) {
-                List<Integer> prev = curr;
-                c.add(curr = new ArrayList<Integer>());
-                for (int j = 0; j <= maxWeight; j++) {
-                    if (j > 0) {
-                        int wH = itemList.get(i-1).getWeight();
-                        curr.add(
-                            (wH > j)
-                            ?
-                            prev.get(j)
-                            :
-                            Math.max(
-                                prev.get(j),
-                                itemList.get(i-1).getValue() + prev.get(j-wH)
-                            )
-                        );
-                    } else {
-                        curr.add(0);
-                    }
-                } // for (j...)
-            } // for (i...)
-            profit = curr.get(maxWeight);
- 
-            for (int i = n, j = maxWeight; i > 0  &&  j >= 0; i--) {
-                int tempI   = c.get(i).get(j);
-                int tempI_1 = c.get(i-1).get(j);
-                if (
-                    (i == 0  &&  tempI > 0)
-                    ||
-                    (i > 0  &&  tempI != tempI_1)
-                )
-                {
-                    Item iH = itemList.get(i-1);
-                    int  wH = iH.getWeight();
-                    iH.setInKnapsack(1);
-                    j -= wH;
-                    solutionWeight += wH;
-                }
-            } // for()
-            calculated = true;
-        } // if()
-        return itemList;
-    }
- 
-    // add an item to the item list
-    public void add(String name, int weight, int value) {
-        if (name.equals(""))
-            name = "" + (itemList.size() + 1);
-        itemList.add(new Item(name, weight, value));
-        setInitialStateForCalculation();
-    }
- 
-    // add an item to the item list
-    public void add(int weight, int value) {
-        add("", weight, value); // the name will be "itemList.size() + 1"!
-    }
- 
-    // remove an item from the item list
-    public void remove(String name) {
-        for (Iterator<Item> it = itemList.iterator(); it.hasNext(); ) {
-            if (name.equals(it.next().getName())) {
-                it.remove();
-            }
-        }
-        setInitialStateForCalculation();
-    }
- 
-    // remove all items from the item list
-    public void removeAllItems() {
-        itemList.clear();
-        setInitialStateForCalculation();
-    }
- 
-    public int getProfit() {
-        if (!calculated)
-            calcSolution();
-        return profit;
-    }
- 
-    public int getSolutionWeight() {return solutionWeight;}
-    public boolean isCalculated() {return calculated;}
-    public int getMaxWeight() {return maxWeight;}
- 
-    public void setMaxWeight(int _maxWeight) {
-        maxWeight = Math.max(_maxWeight, 0);
-    }
- 
-    public void setItemList(List<Item> _itemList) {
-        if (_itemList != null) {
-            itemList = _itemList;
-            for (Item item : _itemList) {
-                item.checkMembers();
-            }
-        }
-    }
- 
-    // set the member with name "inKnapsack" by all items:
-    private void setInKnapsackByAll(int inKnapsack) {
-        for (Item item : itemList)
-            if (inKnapsack > 0)
-                item.setInKnapsack(1);
-            else
-                item.setInKnapsack(0);
-    }
- 
-    // set the data members of class in the state of starting the calculation:
-    protected void setInitialStateForCalculation() {
-        setInKnapsackByAll(0);
-        calculated     = false;
-        profit         = 0;
-        solutionWeight = 0;
-    }
- 
-}
-
+/**
+ * Item which can be packed
+ * 
+ * @author Haijun Su
+ *
+ */
 class Item {
-	 
-    protected String name    = "";
-    protected int weight     = 0;
-    protected int value      = 0;
-    protected int bounding   = 1; // the maximal limit of item's pieces
-    protected int inKnapsack = 0; // the pieces of item in solution
- 
-    public Item() {}
- 
-    public Item(Item item) {
-        setName(item.name);
-        setWeight(item.weight);
-        setValue(item.value);
-        setBounding(item.bounding);
-    }
- 
-    public Item(int _weight, int _value) {
-        setWeight(_weight);
-        setValue(_value);
-    }
- 
-    public Item(int _weight, int _value, int _bounding) {
-        setWeight(_weight);
-        setValue(_value);
-        setBounding(_bounding);
-    }
- 
-    public Item(String _name, int _weight, int _value) {
-        setName(_name);
-        setWeight(_weight);
-        setValue(_value);
-    }
- 
-    public Item(String _name, int _weight, int _value, int _bounding) {
-        setName(_name);
-        setWeight(_weight);
-        setValue(_value);
-        setBounding(_bounding);
-    }
- 
-    public void setName(String _name) {name = _name;}
-    public void setWeight(int _weight) {weight = Math.max(_weight, 0);}
-    public void setValue(int _value) {value = Math.max(_value, 0);}
- 
-    public void setInKnapsack(int _inKnapsack) {
-        inKnapsack = Math.min(getBounding(), Math.max(_inKnapsack, 0));
-    }
- 
-    public void setBounding(int _bounding) {
-        bounding = Math.max(_bounding, 0);
-        if (bounding == 0)
-            inKnapsack = 0;
-    }
- 
-    public void checkMembers() {
-        setWeight(weight);
-        setValue(value);
-        setBounding(bounding);
-        setInKnapsack(inKnapsack);
-    }
- 
-    public String getName() {return name;}
-    public int getWeight() {return weight;}
-    public int getValue() {return value;}
-    public int getInKnapsack() {return inKnapsack;}
-    public int getBounding() {return bounding;}
- 
+
+	private String name;
+	private int weight;
+	private int value;
+
+	/**
+	 * Construct item all properties
+	 * 
+	 * @param name
+	 * @param weight
+	 * @param value
+	 */
+	public Item(String name, int weight, int value) {
+		this.name = name;
+		this.weight = weight;
+		this.value = value;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getWeight() {
+		return weight;
+	}
+
+	public void setWeight(int weight) {
+		this.weight = weight;
+	}
+
+	public int getValue() {
+		return value;
+	}
+
+	public void setValue(int value) {
+		this.value = value;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Item other = (Item) obj;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Item [name=" + name + ", weight=" + weight + ", value=" + value
+				+ "]";
+	}
+
 }
